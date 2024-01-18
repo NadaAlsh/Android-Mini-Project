180ad7c8f12ba64a2a49370a8d6ab5de4d552fed
Nada B E J M A Alshaibani <83315@kfh.com>
2024-01-18 23:00:37 +0300

    Final Commit

--- app/src/main/java/com/nadaalshaibnai/androidminiproject/Classes/TransactionType.java
+++ app/src/main/java/com/nadaalshaibnai/androidminiproject/Classes/TransactionType.java
@@ -3,18 +3,8 @@ package com.nadaalshaibnai.androidminiproject.Classes;
 public enum TransactionType {
     DEPOSIT {
 
-        public String getColor() {
-            return "Green";
-        }
     },
 
     WITHDRAWAL {
 
-        public String getColor() {
-            return "Red";
-        }
-
-
-
     } };
-//
--- app/src/main/java/com/nadaalshaibnai/androidminiproject/MainActivity.java
+++ app/src/main/java/com/nadaalshaibnai/androidminiproject/MainActivity.java
@@ -18,37 +18,25 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements TransactionItemListener {
     public static final String ACCOUNT_NAME_KEY = "ACCOUNT_NAME";
-
     RecyclerView transactionRecyclerView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         transactionRecyclerView = findViewById(R.id.transactionRecyclerView);
-
-
-//        ArrayList<Transaction> transactionList = new ArrayList<>();
 
         transactionRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         TransactionAdapter transactionAdapter = new TransactionAdapter(
                 TransactionRepo.getInstance().generateDummyTransaction(),
                 this
         );
-
         transactionRecyclerView.setAdapter(transactionAdapter);
-
     }
-
     @Override
     public void onTransactionItemClicked(Transaction transaction) {
         Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
         intent.putExtra(ACCOUNT_NAME_KEY, transaction);
         startActivity(intent);
     }
-
-
 }
